fe00de6dabc38fef42c52075435f14509c0eea08
mathieubeden <mbreymand@edenschool.fr>
2020-12-08 16:06:05 +0100

    premiere version totallement fonctionnel

--- blog poo/class.modelet.php
+++ blog poo/class.modelet.php
@@ -2,40 +2,31 @@
 
 class modelet{
     private $id;
+    private $user;
 
-    public function __construct($id){
+    public function __construct($id,$user){
         $this->id=$id;
+        $this->user=$user;
     }
     public function supression(){
         //supression de l'article
-        try{
-            $base = new PDO('mysql:host=127.0.0.1;dbname=blogoo', 'root', '');
-            $base->exec('SET NAMES utf8');
-            $base->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-            $sql = "DELETE FROM `blog` WHERE id=:id";
-            // Préparation de la requête avec les marqueurs
-            $resultat = $base->prepare($sql);
-            $resultat->execute(array('id' => $this->id));
-            catch(Exception $e){
-                // message en cas d'erreur
-                die('Erreur : '.$e->getMessage());
-            }
+        $base = new PDO('mysql:host=127.0.0.1;dbname=blogoo', 'root', '');
+        $base->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $sql = "DELETE FROM blog WHERE id=:id AND user=:user";
+        // Préparation de la requête avec les marqueurs
+        $resultat = $base->prepare($sql);
+        $resultat->execute(array('id' => $this->id,'user'=>$this->user));
+        $resultat->closeCursor();
     }
     public function modifier($titre,$comm){
-            //verification du user et du pass
-        try{
-            $base = new PDO('mysql:host=127.0.0.1;dbname=blogoo', 'root', '');
-            $base->exec('SET NAMES utf8');
-            $base->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-            $sql = "UPDATE `blog` SET titre=:titre,comm=:comm WHERE id=:id";
-            // Préparation de la requête avec les marqueurs
-            $resultat = $base->prepare($sql);
-            $resultat->execute(array('id' => $this->id,'titre' => $titre,'comm' => $comm));
-            catch(Exception $e){
-                // message en cas d'erreur
-                die('Erreur : '.$e->getMessage());
-            }
-    }
+        //verification du user et du pass
+        $base = new PDO('mysql:host=127.0.0.1;dbname=blogoo', 'root', '');
+        $base->exec('SET NAMES utf8');
+        $base->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $sql = "UPDATE `blog` SET titre=:titre,comm=:comm WHERE id=:id and user=:user";
+        // Préparation de la requête avec les marqueurs
+        $resultat = $base->prepare($sql);
+        $resultat->execute(array('id' => $this->id,'titre' => $titre,'comm' => $comm,'user'=>$this->user));
     }
 }
 
--- blog poo/modelet.php
+++ blog poo/modelet.php
@@ -1,26 +1,56 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>modif ou del</title>
+</head>
+<script>
 
+function modoudel(event){
+    if(event.target[1].selected==true){
+       document.getElementById('titre').style.display='none';
+        document.getElementById('comm').style.display='none';
+        document.getElementById('lab1').style.display='none';
+        document.getElementById('lab2').style.display='none';
+    }else  if(event.target[0].selected==true){
+       document.getElementById('titre').style.display='inline-block';
+        document.getElementById('comm').style.display='inline-block';
+        document.getElementById('lab1').style.display='inline-block';
+        document.getElementById('lab2').style.display='inline-block';
+    }
+}
+
+</script>
+<body>
+    
+
+<?php
+include('./class.modelet.php');
 if($_COOKIE['user']==$_GET['user']){
 
-if(isset($_GET['id'])){//pour le form
+    if(!isset($_POST['titre'])){
 
-}
-else{//pour modifier ou deleter
+    echo '<form action="./modelet.php?id='.$_GET['id'].'&user='.$_GET['user'].'" method="POST"><select onchange="modoudel(event);" name = "choix"><option  value = "modif" selected>modifier</option><option value = "delet">supprimer</option></select> <br><label id="lab1" for="titre">titre : </label><br><input type="text" name="titre" id="titre"><br><label  id="lab2" for="comm">commentaire : </label><br><textarea style="width:20%;height:20%;" id="comm" name="comm"></textarea><br><input type="submit" value="soumission"></form>';
 
-    if($_POST['choix']=="delet"){
 
     }
-    else if($_POST['choix']=="modif"){
-        
+    else{//pour modifier ou deleter
+        $model=new modelet($_GET['id'],$_COOKIE['user']);
+        if($_POST['choix']=="delet"){
+            $model->supression();
+            header('location:blogoo.php');
+        }
+        else if($_POST['choix']=="modif"){
+            $model->modifier($_POST['titre'],$_POST['comm']);
+            header('location:blogoo.php');
+        }
+
     }
-
-}
-
-
-
-
 }
 else{
     header('location:connect.php');
 }
 ?>
+</body>
+</html>
